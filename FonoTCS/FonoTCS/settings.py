bb3be9a32f6e091ec9cecad735f7e620c702d822
laragamasantos <laragamasantos@gmail.com>
2024-03-25 19:53:33 -0300

    fixing deployment issues

--- FonoTCS/FonoTCS/settings.py
+++ FonoTCS/FonoTCS/settings.py
@@ -61,7 +61,8 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://fonotcs.medicina.ufmg.br"
 ]
 
 AUTH_USER_MODEL = 'FonoTCSApp.AppUser'
