74bb13a3638ff86f7b89a2560df6766925264f5c
Lara <laragamasantos@gmail.com>
2024-04-20 14:12:40 +0200

    user fields

--- FonoTCS/FonoTCSApp/models.py
+++ FonoTCS/FonoTCSApp/models.py
@@ -4,17 +4,17 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Permi
 from django.contrib.auth.models import Group
 
 class AppUserManager(BaseUserManager):
-	def create_user(self, email, password=None):
+	def create_user(self, email, username, password=None, **extra_fields):
 		if not email:
 			raise ValueError('An email is required.')
-		if not password:
-			raise ValueError('A password is required.')
+		if not username:
+			raise ValueError('A username is required.')
 		email = self.normalize_email(email)
-		user = self.model(email=email)
+		user = self.model(email=email, username=username, **extra_fields)
 		user.set_password(password)
-		user.save()
+		user.save(using=self._db)
 		return user
-	def create_superuser(self, email, password=None):
+	""" def create_superuser(self, email, password=None):
 		if not email:
 			raise ValueError('An email is required.')
 		if not password:
@@ -22,17 +22,23 @@ class AppUserManager(BaseUserManager):
 		user = self.create_user(email, password)
 		user.is_superuser = True
 		user.save()
-		return user
+		return user """
 
 
 class AppUser(AbstractBaseUser, PermissionsMixin):
-    user_id = models.AutoField(primary_key=True)
-    email = models.EmailField(max_length=50, unique=True)
-    username = models.CharField(max_length=50)
-    groups = models.ManyToManyField(Group, related_name='user_groups_app', blank=True)
-    user_permissions = models.ManyToManyField(Permission, related_name='user_permissions_app', blank=True)  
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-    objects = AppUserManager()
-    def __str__(self):
-        return self.username
+	user_id = models.AutoField(primary_key=True)
+	email = models.EmailField(max_length=50, unique=True)
+	username = models.CharField(max_length=50)
+	isTeacher = models.BooleanField(default=False)
+	gender = models.CharField(max_length=20, default='')
+	age = models.IntegerField(default=0)
+	highestDegree = models.CharField(max_length=50, default='')
+	conclusionYear = models.IntegerField(default=0000)
+
+	groups = models.ManyToManyField(Group, related_name='user_groups_app', blank=True)
+	user_permissions = models.ManyToManyField(Permission, related_name='user_permissions_app', blank=True)  
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['username']
+	objects = AppUserManager()
+	def __str__(self):
+		return self.username
--- FonoTCS/FonoTCSApp/serializers.py
+++ FonoTCS/FonoTCSApp/serializers.py
@@ -7,11 +7,18 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = UserModel
 		fields = '__all__'
-	def create(self, clean_data):
-		user_obj = UserModel.objects.create_user(email=clean_data['email'], password=clean_data['password'])
-		user_obj.username = clean_data['username']
-		user_obj.save()
-		return user_obj
+	def create(self, validated_data):
+		user = UserModel.objects.create_user(
+			email=validated_data['email'],
+			username=validated_data['username'],
+			password=validated_data['password'],
+			isTeacher=validated_data.get('isTeacher', False),
+			gender=validated_data.get('gender', ''),
+			age=validated_data.get('age', 0),
+			highestDegree=validated_data.get('highestDegree', ''),
+			conclusionYear=validated_data.get('conclusionYear', 0)
+		)
+		return user
 
 class UserLoginSerializer(serializers.Serializer):
 	email = serializers.EmailField()
